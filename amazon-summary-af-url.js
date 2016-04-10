afec50ee2b5bb36ba4c78a8ddda8ce966d72c96a
Yuki Fujiwara <sky.y.0079@gmail.com>
2016-04-10 23:02:35 +0900

    Initial commit

---
+++ amazon-summary-af-url.js
@@ -0,0 +1,8 @@
+(function(){
+  var id = "skyy0079-22"
+
+  if (location.href.search(/[^0-9A-Z]([B0-9][0-9A-Z]{9})([^0-9A-Z]|$)/) != -1) {
+    var u ='http://www.amazon.co.jp/dp/'+RegExp.$1+'/'+id;
+    prompt('Copy Amazon Short URL',u);
+  }
+}());
