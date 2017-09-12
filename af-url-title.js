6d7f1964e5d0b1b45d7b7c1b92c929a8a3ad29d9
Yuki Fujiwara <sky.y.0079@gmail.com>
2017-09-12 18:08:08 +0900

    Removed my affliate id.

--- af-url-title.js
+++ af-url-title.js
@@ -1,6 +1,6 @@
 (function(){
-  var id = "skyy0079-22"
-
+  var id = "YOUR_AMAZON_AFFILIATE_ID";
+  
   if (location.href.search(/[^0-9A-Z]([B0-9][0-9A-Z]{9})([^0-9A-Z]|$)/) != -1) {
     var u ='http://' + location.host + '/dp/'+RegExp.$1+'/' + id;
     prompt('Copy Amazon Short URL',document.title + " " + u);
