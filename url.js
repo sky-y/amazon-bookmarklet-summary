d24a16567656c5ea53fd00393b22b2a5b358b72c
Yuki Fujiwara <sky.y.0079@gmail.com>
2018-07-29 11:24:23 +0900

    init

---
+++ url.js
@@ -0,0 +1,6 @@
+(function(){
+  if (location.href.search(/[^0-9A-Z]([B0-9][0-9A-Z]{9})([^0-9A-Z]|$)/) != -1) {
+    var u ='http://' + location.host + '/dp/' + RegExp.$1 + '/';
+    prompt('Copy Amazon Short URL',u);
+  }
+}());
